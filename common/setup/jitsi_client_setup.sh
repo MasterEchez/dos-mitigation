df77f402acdb4287a85b93dfb389c951b134ae56
MasterEchez <eechezona9@gmail.com>
2025-01-28 02:55:06 -0500

    add installation of puppeteer to client setup

--- common/setup/jitsi_client_setup.sh
+++ common/setup/jitsi_client_setup.sh
@@ -5,3 +5,8 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 yes | sudo apt --fix-broken install
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# puppeteer installation
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+nvm install --lts
+npm install puppeteer
