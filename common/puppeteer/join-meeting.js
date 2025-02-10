70db19e8e29afa3909528cdb95e93c283fc3030d
MasterEchez <eechezona9@gmail.com>
2025-02-09 19:36:52 -0500

    to pre-meeting screen

--- common/puppeteer/join-meeting.js
+++ common/puppeteer/join-meeting.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const puppeteer = require('puppeteer');
+const process = require('process');
+const [ clientName ] = process.argv.slice(2);
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -10,16 +12,21 @@ const puppeteer = require('puppeteer');
 
     await page.goto('https://s0');
     let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+    await page.screenshot({path: 'home.png', fullPage: true});
     // console.log(bodyHTML);
     await page.type('#enter_room_field', 'dos-miti');
 
-    await page.click('#enter_room_button');
-
-    // await page.waitForNavigation();
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('#enter_room_button'),
+    ]);
 
     console.log('New Page URL:', page.url());
 
+    bodyHTML = await page.evaluate(() => document.body.innerHTML);
+    await page.type('#premeeting-name-input', clientName);
     await page.screenshot({path: 'meeting.png', fullPage: true});
+    console.log(bodyHTML);
 
     await browser.close();
 })();
