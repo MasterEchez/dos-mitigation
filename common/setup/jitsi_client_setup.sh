adb5cdfd799c6fd6421756005e0fc91c245d3e81
MasterEchez <eechezona9@gmail.com>
2025-02-14 18:20:34 -0500

    clean up jitsi setup

--- common/setup/jitsi_client_setup.sh
+++ common/setup/jitsi_client_setup.sh
@@ -7,4 +7,9 @@ yes | sudo apt --fix-broken install
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # puppeteer installation (part 1)
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install --lts
+npm install puppeteer
--- common/setup/jitsi_client_setup_part2.sh
+++
@@ -1,4 +0,0 @@
-# puppeteer installation (part 2)
-nvm install --lts
-npm install puppeteer
-cp -r /usr/local/dos-mitigation/common/puppeteer/ ./
