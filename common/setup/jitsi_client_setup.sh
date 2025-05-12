4452fd7b6fa5e1db9be2eb190c3cc44d39b530a3
MasterEchez <eechezona9@gmail.com>
2025-05-11 22:52:40 -0400

    download four people video in jitsi_client_setup script

--- common/setup/jitsi_client_setup.sh
+++ common/setup/jitsi_client_setup.sh
@@ -6,6 +6,10 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 yes | sudo apt --fix-broken install
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
+# four people download
+FOUR_PEOPLE_Y4M="fourpeople.y4m"
+curl -s https://media.xiph.org/video/derf/y4m/FourPeople_1280x720_60.y4m -o $FOUR_PEOPLE_Y4M
+
 # puppeteer installation (part 1)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
