bb32ba137b7cc6a7b772447fa013f8b5a60b4cf2
MasterEchez <eechezona9@gmail.com>
2025-06-14 15:43:37 -0400

    other media files

--- common/setup/jitsi_client_setup.sh
+++ common/setup/jitsi_client_setup.sh
@@ -6,9 +6,16 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 yes | sudo apt --fix-broken install
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-# four people download
+# four people download / build repeated
 FOUR_PEOPLE_Y4M="fourpeople.y4m"
+FOUR_PEOPLE_LOOPED_Y4M="fourpeople_looped.y4m"
 curl -s https://media.xiph.org/video/derf/y4m/FourPeople_1280x720_60.y4m -o $FOUR_PEOPLE_Y4M
+for i in {1..5}; do printf "file '%s'\n" $FOUR_PEOPLE_Y4M >> list.txt; done
+ffmpeg -f concat -i list.txt -f yuv4mpegpipe fourpeople_looped.y4m
+
+# sintel trailer download
+SINTEL_TRAILER_Y4M="sinteltrailer.y4m"
+curl -s https://media.xiph.org/video/derf/y4m/sintel_trailer_2k_720p24.y4m -o $SINTEL_TRAILER_Y4M
 
 # puppeteer installation (part 1)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
